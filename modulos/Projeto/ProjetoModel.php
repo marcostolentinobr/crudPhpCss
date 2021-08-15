2f4491f08f15bd829feb3e228bf2f8cf9be2e39d
Marcos Tolentino <marcostolentino@outlook.com>
2021-08-15 20:07:09 -0300

    projeto model

--- modulos/Projeto/ProjetoModel.php
+++ modulos/Projeto/ProjetoModel.php
@@ -1,81 +1,34 @@
 <?php
 
-require_once RAIZ . '/modulos/Model.php';
-
 class ProjetoModel extends Model
 {
+
+    protected $tabela = 'PROJETO';
+
     //select
-    private $select = '
+    protected $select = '
         SELECT 
         
-               -- atividade 
+               -- projeto
                P.id,
                P.nome,
                P.data_inicio,
                P.data_fim,
                P.data_concluido
-     
+
           FROM PROJETO P
     ';
 
-    private $select_edit = '
+    protected $select_edit = '
         SELECT 
-               -- atividade 
+               -- projeto
                P.id,
                P.nome,
                P.data_inicio,
                P.data_fim,
-               P.data_concluido,
-               P.projeto_id       
+               P.data_concluido    
                
-          FROM PROPJETO P
+          FROM PROJETO P
          WHERE P.id = :id
     ';
-
-    //list
-    public function list($where = [])
-    {
-        if ($where) {
-            return $this->all($this->select_edit, $where);
-        }
-
-        return $this->all($this->select);
-    }
-
-    //insert
-    public function insert($line)
-    {
-        $insert = '
-            INSERT INTO PROJETO 
-            ( nome,  data_inicio,  data_fim) VALUES 
-            (:nome, :data_inicio, :data_fim)
-        ';
-        return $this->prepExec($insert,  $line);
-    }
-
-    //delete
-    public function delete($where)
-    {
-        $delete = '
-            DELETE 
-              FROM PROJETO
-             WHERE id = :id
-        ';
-
-        return $this->prepExec($delete, $where);
-    }
-
-    //update
-    public function update($line, $where)
-    {
-        $delete = '
-            UPDATE PROJETO 
-               SET nome        = :nome,
-                   data_inicio = :data_inicio,
-                   data_fim    = :data_fim
-             WHERE id = :id
-        ';
-
-        return $this->prepExec($delete, array_merge($line, $where));
-    }
 }
